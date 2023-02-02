efde4c92b9a012b8e5f5e9bebb7aef491a35877b
mcflyspb <mcflyspb@gmail.com>
2023-02-02 09:24:49 +0300

    Test project for learning class inheritance.

--- src/ru/linevo/mypakage/Main.java
+++ src/ru/linevo/mypakage/Main.java
@@ -1,7 +1,37 @@
 package ru.linevo.mypakage;
 
 public class Main {
+    enum Direction {
+        EAST("E"),
+        WEST("W"),
+        NORTH("N") {
+            public String getShortCode() {
+                return "TEST";
+            }
+        },
+        SOUTH("S");
+
+        private final String shortCode;
+
+        Direction(String code) {
+            this.shortCode = code;
+        }
+
+        public String getShortCode() {
+            return this.shortCode;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String a = Direction.NORTH.getShortCode();
+        System.out.println(a);
+
+        System.out.println(Direction.valueOf("NORTH"));
+
+        System.out.println(Direction.NORTH.name());
+
+        System.out.println(Direction.NORTH.toString());
+
+
+
     }
 }
